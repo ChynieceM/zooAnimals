9ca22a04d210238f4fbc2fa3122471905bb44ec7
Chyniece Matthews <cfmatthews2@gmail.com>
2023-07-13 10:16:52 -0500

    extra credit

---
+++ zooAnimals/zooAnimals/Class1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+namespace zooAnimals
+{
+    public class Animal
+    {
+        public string Name { get; set; }
+        public string Color { get; set; }
+        public int Legs { get; set; }
+
+        public bool IsSleeping { get; set; }
+        public bool IsEating { get; set; }
+
+        //public Animal(string name, string color, int legs ) {
+
+        //    Name = name;
+        //    Color = color;
+        //    Legs = legs;
+
+        //}
+        public Animal() { }
+        public bool Eat()
+        {
+            IsEating = true;
+
+            return IsEating;
+        }
+
+        public bool Sleep()
+        {
+            IsSleeping = true;
+
+            return IsSleeping;
+        }
+    }
+}
---
+++ zooAnimals/zooAnimals/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace zooAnimals
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal Peacock = new Animal();
+
+            Peacock.Name = "Peacock";
+            Peacock.Color = "rainbow";
+            Peacock.Legs = 2;
+            Peacock.IsEating = true;
+            Peacock.IsSleeping = false;
+          
+
+            Console.WriteLine($"This animal is a {Peacock.Name} with {Peacock.Legs} legs and is {Peacock.Color} colored. Is Eating? {true}. Is Sleeping? {false}" );
+            Console.ReadLine();
+
+            Animal Elephant = new Animal();
+
+            Elephant.Name = "Elephant";
+            Elephant.Color = "grey";
+            Elephant.Legs = 4;
+            Elephant.IsEating = false;
+            Elephant.IsSleeping = true;
+
+            Console.WriteLine($"This animal is a {Elephant.Name} with {Elephant.Legs} legs and is {Elephant.Color}. Is Eating? {false} . Is Sleeping?  {true}");
+            Console.ReadLine();
+
+            Animal TRex = new Animal();
+
+            TRex.Name = "T-Rex";
+            TRex.Color = "Red and Blue";
+            TRex.Legs = 4;
+            TRex.IsEating = true;
+            TRex.IsSleeping = false;
+
+            Console.WriteLine($"This animal is a {TRex.Name} with {TRex.Legs}and is {TRex.Color}. Is Eating? {true}. Is Sleeping? {false}");
+            Console.ReadLine();
+        }
+    }
+}
